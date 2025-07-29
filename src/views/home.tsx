e1e9d999a32c1aacea37c2401ee7ca667c87a3a3
Aaron-Ccente <aronccente@gmail.com>
2025-07-29 12:47:27 -0500

    segunda seccion - categorias

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,9 +1,11 @@
+import Categories from '../components/sections/Categories'
 import Inicio from '../components/sections/Inicio'
 
 function Home() {
   return (
-    <div className="min-h-screen bg-fondoPrincipal dark:bg-fondoPrincipal-dark dark:text-texto-dark grid place-items-center">
+    <div className="min-h-screen bg-fondoPrincipal dark:bg-fondoPrincipal-dark dark:text-texto-dark grid place-items-center py-4">
       <Inicio/>
+      <Categories/>
     </div>
   )
 }
